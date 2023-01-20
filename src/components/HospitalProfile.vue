a2dbfef6fef54f816bb344d19c22dda926a4af5f
daniel-samuel <folarinded@gmail.com>
2023-01-20 15:56:40 +0100

    haii

--- src/components/HospitalProfile.vue
+++ src/components/HospitalProfile.vue
@@ -3,16 +3,16 @@
     class="w-full md:w-4/12 bg-white border-b-2 rounded-lg p-6 col-span-2 md:col-span-1"
   >
     <h3 class="text-[#FF5200] text-md mb-8 font-semibold">Hospital Profile</h3>
-    <div v-for="hos in hospital" :key="hos._id">
+    <div>
       <div class="flex flex-col space-y-4">
         <div>
           <p class="text-base text-[#25282B]">Hospital Name</p>
-          <p class="text-sm text-[#52575C]">{{ hos.name }}</p>
+          <p class="text-sm text-[#52575C]">{{ hosiptal.name }}</p>
         </div>
         <div>
           <p class="text-base text-[#25282B]">Address</p>
           <p class="text-sm text-[#52575C]">
-            {{ hos.address }}
+            {{ hospital.address }}
           </p>
         </div>
         <div>
