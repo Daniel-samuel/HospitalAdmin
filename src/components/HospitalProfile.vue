bc84badc2d152c2a839bc13c2ce6e17cbde36ec1
daniel-samuel <folarinded@gmail.com>
2023-01-20 08:10:43 +0100

    haii

--- src/components/HospitalProfile.vue
+++ src/components/HospitalProfile.vue
@@ -3,27 +3,29 @@
     class="w-full md:w-4/12 bg-white border-b-2 rounded-lg p-6 col-span-2 md:col-span-1"
   >
     <h3 class="text-[#FF5200] text-md mb-8 font-semibold">Hospital Profile</h3>
-    <div v-for="hos in hospital" :key="hos._id" class="flex flex-col space-y-4">
-      <div>
-        <p class="text-base text-[#25282B]">Hospital Name</p>
-        <p class="text-sm text-[#52575C]">{{ hos.name }}</p>
-      </div>
-      <div>
-        <p class="text-base text-[#25282B]">Address</p>
-        <p class="text-sm text-[#52575C]">
-          {{ hos.address }}
-        </p>
-      </div>
-      <div>
-        <p class="text-base text-[#25282B]">Hospital License</p>
-        <p class="text-sm text-[#52575C]">XXE445KLW/LGA/NG</p>
-      </div>
-      <div>
-        <p class="text-base text-[#25282B]">About</p>
-        <p class="text-sm text-[#52575C]">
-          The Reddington Healthcare Group is renowned at home and abroad for
-          medical excellence and attracts world-class specialist Doctors
-        </p>
+    <div v-for="hos in hospital" :key="hos._id">
+      <div class="flex flex-col space-y-4">
+        <div>
+          <p class="text-base text-[#25282B]">Hospital Name</p>
+          <p class="text-sm text-[#52575C]">{{ hos.name }}</p>
+        </div>
+        <div>
+          <p class="text-base text-[#25282B]">Address</p>
+          <p class="text-sm text-[#52575C]">
+            {{ hos.address }}
+          </p>
+        </div>
+        <div>
+          <p class="text-base text-[#25282B]">Hospital License</p>
+          <p class="text-sm text-[#52575C]">XXE445KLW/LGA/NG</p>
+        </div>
+        <div>
+          <p class="text-base text-[#25282B]">About</p>
+          <p class="text-sm text-[#52575C]">
+            The Reddington Healthcare Group is renowned at home and abroad for
+            medical excellence and attracts world-class specialist Doctors
+          </p>
+        </div>
       </div>
     </div>
   </div>
